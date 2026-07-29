db8fb47c52ad87933a5226b88c10558d49b3962d
SravanPKumar <sp.pathiyoor@gmail.com>
2026-07-29 10:47:32 +0530

    last edit apply + recruiter1

--- project/web/services/gmail_service.py
+++ project/web/services/gmail_service.py
@@ -12,6 +12,7 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 from google_auth_oauthlib.flow import Flow
+#test
 
 from ..models import GmailConnection
 
